e35615d95629364fb980f128535f616f3c7f3717
DhesikaRamesh <dhesikadee@gmail.com>
2025-11-20 22:35:47 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      
+    
+      colors: {
+        secondary: "#1b2d77",
+      },
+      fontFamily : {
+        'hero-font' : 'BBH+Sans+Hegarty'
+      },
+      borderColor :{
+        'primary' : 'rgb(85 81 227)',
+        'secondary' : '#2b2d77',
+
+      
+        
+    },
+  },
+  plugins: [],
+}
+}
+
